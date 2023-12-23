1488e1adca095c2d03fc3fdc1cbec0d5071bfa45
Eren <gokyildizsemieren@gmail.com>
2023-12-23 20:14:48 +0300

    fix code,

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ void TIMER_Config()
 	TIM_TimeBaseInitTypeDef TIM_InitStruct;
 	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;        //->Divide APB1 Timer clock by 1.
 	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up;    //->The Settings of Counter is Up.
-	TIM_InitStruct.TIM_Period = 3999;                      	//->The Value of Auto-Reload Register is 1999.
+	TIM_InitStruct.TIM_Period = 3999;                      	//->The Value of Auto-Reload Register is 3999.
 	TIM_InitStruct.TIM_Prescaler = 41999;                   //->The Value of Prescaler is 41999.
 	TIM_InitStruct.TIM_RepetitionCounter = 0;               //->Number of repetitions is 1 (n+1).
 
